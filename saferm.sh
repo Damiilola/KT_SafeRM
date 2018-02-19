5ccb65ab6acd42d64f01b8ebae53fc50594eec4d
Dami Adenuga <damilola.adenuga@MacBook-Pro-2.domain.name>
2018-02-19 17:40:55 +0100

    .

--- saferm.sh
+++ saferm.sh
@@ -1,1 +1,48 @@
-!#/bin/bash
+#!/bin/bash
+#This is a bash script.
+
+#create a .Trash_saferm directory
+
+home="$HOME"
+trashSafermDirName=".Trash_saferm"
+trashSafermPath="$home/$trashSafermDirName"
+
+
+trashsafermfunction(){
+    if [ ! -d "$HOME/.Trash_saferm" ];
+    then
+        mkdir "$HOME/.Trash_saferm"
+        echo "$trashSafeDirName created"
+    else
+        echo "$trashSafeDirName already exists"
+    fi
+}
+
+#making if statements for the interactive thing
+#first check if it is file or a directory
+
+
+
+
+if [[ -f "$1" ]]
+then
+    read -p "remove $1? " reply
+
+    if [[ "$reply" == 'y' || "$reply" == 'Y' ]]
+    then
+        mv $1 $trashSafermPath
+        echo "File has been removed"
+    else
+        echo "File has not been removed"
+fi
+#
+#if [[ -d "$1"]]
+#then
+#    read -p "remove $1? " reply
+#
+#    if [[ "$reply" == 'y' || "$reply" == 'Y' ]]
+#    then
+#        mv $1 $trashSafermPath
+#        echo "File has been removed"
+#    else
+#        echo "File has not been removed"
